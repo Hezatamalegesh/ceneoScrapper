7ff6696e04a67a23b31ca4b3c633e6a72f93941d
Hezatamalegesh <dawthemaster@gmail.com>
2023-03-24 12:49:03 +0100

    2023-03-24 DM

---
+++ scraper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+product_code = input("Podaj kod produktu: ")
+url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+respons = requests.get(url)
+if respons.status_code == requests.codes.ok:
+    soup = BeautifulSoup(respons.text, 'html.parser')
+    opinions = soup.select("div.js_product-review")
+    if len(opinions) > 0:
+        opinions_all = []
+        for opinion in opinions:
+            single_opinion = {
+                "opinion_id": ,
+                "author": ,
+                "recommendation": ,
+                "stars": ,
+                "purchased": ,
+                "opinion_date": ,
+                "purchase_date": ,
+                "useful_count": ,
+                "useless_count": ,
+                "content": ,
+                "pros": ,
+                "cons":
+            }
+    else:
+        print("Nie ma opinii")
